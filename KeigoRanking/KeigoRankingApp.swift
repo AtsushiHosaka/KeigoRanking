c5385c91a2af7092fe64e696b76b8ccba6e1b9e7
Atsushi Hosaka <hosaka2917@gmail.com>
2024-05-16 11:31:57 +0900

    Initial Commit

---
+++ KeigoRanking/KeigoRankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeigoRankingApp.swift
+//  KeigoRanking
+//
+//  Created by 保坂篤志 on 2024/05/16.
+//
+
+import SwiftUI
+
+@main
+struct KeigoRankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
